831edc2c38ee2d3ade149d7a39f688cbf81c55ec
Swetha <Swetha@192.168.0.10>
2020-04-09 01:55:53 +0530

    Commit 1 - Selenium - base test

---
+++ src/test/java/com/swetha/base/TestBase.java
@@ -0,0 +1,22 @@
+package com.swetha.base;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeSuite;
+
+public class TestBase {
+
+	public static WebDriver driver;
+	
+	
+	
+	
+	@BeforeSuite
+	public void setUp() {
+		
+	}
+	
+	public void tearDown() {
+		
+	}
+	
+}
